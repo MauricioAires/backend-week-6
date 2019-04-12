11ac86d7c6787a440495ee4d44f52a2740063a68
MauricioAires <emailescolar24@gmail.com>
2019-04-11 21:17:16 -0300

    bug corrigido

--- src/config/multer.js
+++ src/config/multer.js
@@ -6,15 +6,15 @@ module.exports = {
  dest: path.resolve(__dirname, '..', '..', 'tmp'),
  
  storage: multer.diskStorage({
-     destination: (req,file,cd) => {
+     destination: (req,file,cb) => {
 
-         cd(null,path.resolve(__dirname, '..', '..', 'tmp'));
+         cb(null,path.resolve(__dirname, '..', '..', 'tmp'));
      },
      filename: (req,file,cb) => {
         crypto.randomBytes(16,( err, hash ) => {
             if(err) cb(err);
 
-            file.key = `${hash.toString('hex')}-${file.originalname}`
+            file.key = `${hash.toString('hex')}-${file.originalname}`;
 
             cb(null, file.key)
         }); 
--- src/server.js
+++ src/server.js
@@ -39,6 +39,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-server.listen(3333,() => {
-    console.log(" Server start at port 3333 ")
-});
+server.listen( process.env.PORT || 3333 );
